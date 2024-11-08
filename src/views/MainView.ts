b00668620a4c2df1d98b204e7ae9b59bd8c79603
Birdman1104 <arsenmazmanyan11@gmail.com>
2024-11-08 18:19:53 +0400

    Added collision with pipes and ground.

--- src/views/MainView.ts
+++ src/views/MainView.ts
@@ -5,6 +5,9 @@ export class MainView extends Phaser.GameObjects.Container {
   private bkg: Phaser.GameObjects.TileSprite;
   private bird: Phaser.GameObjects.Image;
   private pipes: PipeContainer[] = [];
+  private overlap: Phaser.Physics.Arcade.Collider;
+
+  private isLost = false;
 
   public constructor(scene: Phaser.Scene) {
     super(scene);
@@ -12,20 +15,25 @@ export class MainView extends Phaser.GameObjects.Container {
   }
 
   public update(): void {
+    const { height } = this.scene.scale;
+    if (this.bird && this.bird.y > height - this.bird.height / 2) {
+      this.scene.physics.world.disable(this.bird);
+      this.disableActions();
+    }
+
+    if (this.isLost) {
+      return;
+    }
+
     this.pipes.forEach((p, i) => {
       p.x -= CONFIGS.speed;
       if (p.x <= -26) {
         p.destroy();
         this.pipes.splice(i, 1);
         this.addPipe();
+        this.addOverlap();
       }
     });
-
-    const { height } = this.scene.scale;
-    if (this.bird && this.bird.y > height - this.bird.height / 2) {
-      this.scene.physics.world.disable(this.bird);
-      this.bkg.disableInteractive();
-    }
 
     this.bkg.tilePositionX += CONFIGS.speed;
   }
@@ -37,6 +45,8 @@ export class MainView extends Phaser.GameObjects.Container {
     this.addPipe();
     this.addPipe();
     this.addPipe();
+
+    this.addOverlap();
   }
 
   private buildBkg(): void {
@@ -60,6 +70,17 @@ export class MainView extends Phaser.GameObjects.Container {
     pipe.x = pipeX;
     this.add(pipe);
     this.pipes.push(pipe);
+  }
+
+  private addOverlap(): void {
+    const allPipes = this.pipes.map((p) => p.pipes).flat();
+    this.overlap?.destroy();
+    this.overlap = this.scene.physics.add.overlap(this.bird, allPipes, () => this.disableActions());
+  }
+
+  private disableActions(): void {
+    this.isLost = true;
+    this.bkg.disableInteractive();
   }
 
   private onBkgClick(): void {
--- src/views/PipeContainer.ts
+++ src/views/PipeContainer.ts
@@ -1,20 +1,31 @@
 export class PipeContainer extends Phaser.GameObjects.Container {
+  private pipeTop: Phaser.Physics.Arcade.Sprite;
+  private pipeBottom: Phaser.Physics.Arcade.Sprite;
+
   public constructor(scene: Phaser.Scene) {
     super(scene);
     this.init();
   }
 
+  public get pipes(): Phaser.Physics.Arcade.Sprite[] {
+    return [this.pipeTop, this.pipeBottom];
+  }
+
   private init(): void {
     const pipe1Y = Math.random() * 150 + 400;
-    const pipe1 = this.scene.physics.add.sprite(0, pipe1Y, "pipe");
-    pipe1.body.allowGravity = false;
+    this.pipeBottom = this.scene.physics.add.sprite(0, pipe1Y, "pipe");
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.pipeBottom.body.allowGravity = false;
 
     const pipe2Y = Math.random() * 50 - 50;
-    const pipe2 = this.scene.physics.add.sprite(0, pipe2Y, "pipe");
-    pipe2.body.allowGravity = false;
-    pipe2.scaleY = -1;
+    this.pipeTop = this.scene.physics.add.sprite(0, pipe2Y, "pipe");
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    this.pipeTop.body.allowGravity = false;
+    this.pipeTop.scaleY = -1;
 
-    this.add(pipe1);
-    this.add(pipe2);
+    this.add(this.pipeBottom);
+    this.add(this.pipeTop);
   }
 }
